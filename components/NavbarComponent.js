bb650f0ad69c1b7a74c5d76c7f44f0b7b801b8b4
Sean Knowles <s@seanknowles.dev>
2021-03-12 22:03:51 +0000

    minor directory changes and file deletions

--- components/NavbarComponent.js
+++ components/NavbarComponent.js
@@ -3,6 +3,8 @@ function Navbar(path) {
   <nav aria-label="breadcrumb" class="row p-3">
     <a href="/">Home</a>
     <a href="#/profile">Profile</a>
+    <a href="#/Search">Search</a>
+    <a href="#/Album">Album</a>
   </nav>
   `
 }
--- components/Sidenav.js
+++
@@ -1,8 +0,0 @@
-export function Sidenav() {
-  return /*html*/ `
-  <nav aria-label="breadcrumb" class="row p-3">
-    <a href="/">Home</a>
-    <a href="#/search">Search</a>
-  </nav>
-  `
-}
